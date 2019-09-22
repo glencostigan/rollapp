35d4fcf9cb240e40bdc853a6f519e9a8ca542359
Glen Costigan <glen.costigan@gmail.com>
2019-09-22 01:44:02 -0400

    add dice roll

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-wrapper";
 
+state = {
+  numberOfDice: null,
+  rolls: [],
+  rollSum: null
+};
+diceRoll = numberOfDice => {
+  let rolls = [];
+  let rollSum = 0;
+  for (let i = 0; i < numberOfDice; i++) {
+    rolls[i] = Math.floor(Math.random() * 6) + 1;
+    rollSum += rolls[i];
+  }
+  this.setState({
+    numberOfDice,
+    rolls,
+    rollSum
+  });
+};
+
 function App() {
   const { loading } = useAuth0();
 
@@ -18,6 +37,20 @@ function App() {
       <header>
         <NavBar />
       </header>
+    </div>
+    <div className="buttons">
+      {[1, 2, 3, 4, 5].map(number => {
+        let text = number === 1 ? "die" : "dice";
+        return (
+          <button
+            key={number}
+            onClick={() => this.diceRoll(number)}
+            className="button"
+          >
+            {number} {text}
+          </button>
+        );
+      })}
     </div>
   );
 }
